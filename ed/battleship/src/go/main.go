50d956f159f2319fcb32b28d8c8d82450bd5a490
André Cayare <acayaresousa@gmail.com>
2026-05-27 23:30:09 -0300

    att

---
+++ ed/battleship/src/go/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// Função que será chamada no LeetCode
+func countBattleships(board [][]byte) int {
+	totalNavios := 0
+	totalLinhas := len(board)
+	if totalLinhas == 0 {
+		return 0
+	}
+	totalColunas := len(board[0])
+
+	for linha := 0; linha < totalLinhas; linha++ {
+		for coluna := 0; coluna < totalColunas; coluna++ {
+			if board[linha][coluna] == '.' {
+				continue
+			}
+			if linha > 0 && board[linha-1][coluna] == 'X' {
+				continue
+			}
+			//if coluna > 0 && board[linha][coluna-1] == 'X' {
+			//	continue
+			//}
+			totalNavios++
+		}
+	}
+
+	return totalNavios
+
+}
+
+// Não modifique a função main
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	line := scanner.Text()
+	var nl, nc int
+	fmt.Sscanf(line, "%d %d", &nl, &nc)
+
+	board := make([][]byte, nl)
+	for i := 0; i < nl; i++ {
+		scanner.Scan()
+		board[i] = []byte(scanner.Text())
+	}
+
+	result := countBattleships(board)
+	fmt.Println(result)
+}
--- ed/longest/src/go/main.go
+++ ed/longest/src/go/main.go
@@ -8,10 +8,56 @@ import (
 	"strings"
 )
 
-func longestIncreasingPath(matrix [][]int) int {
-	//
-	_ := matrix
-	return 0
+var direcoes = [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+func buscarMaiorCaminho(matriz [][]int, blocoDeNotas [][]int, linha int, coluna int) int {
+	if blocoDeNotas[linha][coluna] != 0 {
+		return blocoDeNotas[linha][coluna]
+	}
+
+	tamanhoMaximo := 1
+
+	for _, dir := range direcoes {
+		proximaLinha := linha + dir[0]
+		proximaColuna := coluna + dir[1]
+
+		if proximaLinha >= 0 && proximaColuna >= 0 && proximaLinha < len(matriz) && proximaColuna < len(matriz[0]) {
+			if matriz[proximaLinha][proximaColuna] > matriz[linha][coluna] {
+				comprimentoDoCaminho := 1 + buscarMaiorCaminho(matriz, blocoDeNotas, proximaLinha, proximaColuna)
+				if comprimentoDoCaminho > tamanhoMaximo {
+					tamanhoMaximo = comprimentoDoCaminho
+				}
+			}
+		}
+	}
+	blocoDeNotas[linha][coluna] = tamanhoMaximo
+	return tamanhoMaximo
+}
+
+func encontrarCaminhoCrescenteMaisLongo(matriz [][]int) int {
+	if len(matriz) == 0 || len(matriz[0]) == 0 {
+		return 0
+	}
+
+	totalLinhas := len(matriz)
+	totalColunas := len(matriz[0])
+
+	blocoDeNotas := make([][]int, totalLinhas)
+	for i := range blocoDeNotas {
+		blocoDeNotas[i] = make([]int, totalColunas)
+	}
+
+	maiorCaminhoGeral := 0
+
+	for i := 0; i < totalLinhas; i++ {
+		for j := 0; j < totalColunas; j++ {
+			caminhoAtual := buscarMaiorCaminho(matriz, blocoDeNotas, i, j)
+			if caminhoAtual > maiorCaminhoGeral {
+				maiorCaminhoGeral = caminhoAtual
+			}
+		}
+	}
+	return maiorCaminhoGeral
 }
 
 // Não modifique a função main
@@ -42,5 +88,5 @@ func main() {
 		matrix[i] = row
 	}
 
-	fmt.Println(longestIncreasingPath(matrix))
+	fmt.Println(encontrarCaminhoCrescenteMaisLongo(matrix))
 }
